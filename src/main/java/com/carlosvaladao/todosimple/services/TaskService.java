10665f6a71b1c3d5692908a96f1f069bb2240fad
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-08-17 16:51:30 -0300

    Create TaskService

---
+++ src/main/java/com/carlosvaladao/todosimple/services/TaskService.java
@@ -0,0 +1,51 @@
+package com.carlosvaladao.todosimple.services;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carlosvaladao.todosimple.models.Task;
+import com.carlosvaladao.todosimple.models.User;
+import com.carlosvaladao.todosimple.repositories.TaskRepository;
+
+@Service
+public class TaskService {
+    
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserService userService;
+
+    public Task findById(Long id) {
+        Optional<Task> task = this.taskRepository.findById(id);
+        return task.orElseThrow(() -> new RuntimeException(
+            "Tarefa não encontrada! Id: " + id + ", Tipo: " + Task.class.getName()
+        ));
+    }
+
+    @Transactional
+    public Task create(Task obj) {
+        User user = this.userService.findById(obj.getUser().getId());
+        obj.setId(null);
+        obj.setUser(user);
+        obj = this.taskRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public Task update(Task obj) {
+        Task newObj = findById(obj.getId());
+        newObj.setDescription(obj.getDescription());
+        return this.taskRepository.save(obj);
+    }
+
+    @Transactional
+    public void delete(Long task_id) {
+        Task task = findById(task_id);
+        this.taskRepository.delete(task);
+    }
+}
