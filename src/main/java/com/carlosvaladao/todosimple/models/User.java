f64a7ebc8837ff07235868fcdc625436cb747b62
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2025-08-17 13:31:46 -0300

    Add User tasks get and setter

--- src/main/java/com/carlosvaladao/todosimple/models/User.java
+++ src/main/java/com/carlosvaladao/todosimple/models/User.java
@@ -99,6 +99,14 @@ public class User {
         return result;
     }
 
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
